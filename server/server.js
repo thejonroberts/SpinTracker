32a6fad65cc76c97b83eaf7342f6d19643167c40
thejonroberts <jon.roberts.8@gmail.com>
2017-08-07 14:33:41 -0500

    grab complete html file and write to folder

--- server/server.js
+++ server/server.js
@@ -1,10 +1,48 @@
-'use strict';
+require('dotenv').config();
+const fs = require('fs'),
+      request = require('request'),
+      cheerio = require('cheerio'),
+      pageURL = 'http://www.themutinychicago.com/',
+      host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+      port = process.env.PORT || 8080,
+      cors_proxy = require('cors-anywhere'),
+      // twilio = require('twilio'),
+      // client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN),
+      Storage = require('node-storage'),
+      store = new Storage(__dirname + "/data/scrapedData.js"),
+      // origPrice = null;
 
-const request = require('request'),
-			express = require('express'),
-			cheerio = require('cheerio'),
-			cors_proxy = require('cors-anywhere'),
-			app = express();
+cors_proxy.createServer({
+  originWhitelist: [], // Allow all origins
+  removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+  console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
 
-const PORT = process.env.PORT || 3000;
+function scrapePage() {
+  return new Promise( (resolve, reject) => {
+    //make an HTTP request for the page to be scraped
+    request(`http://localhost:${port}/${pageURL}`, (error, response, responseHtml) => {
+      if (error) { console.log("error", error); reject(); };
+      //write the entire scraped page to the local file system
+      fs.writeFile(__dirname + '/html/scrapedPage.html', responseHtml, (err) => {
+          if (err) console.log("error in write file", err);
+          console.log('entire-page.html successfully written to HTML folder');
+          resolve(responseHtml)
+      })
+    })
+  })
+}
 
+//scrape the page
+scrapePage()
+.then( (resHtml) => {
+	console.log('html written to folder', resHtml);
+  // return parseItems(resHtml)
+})
+// .then( ({currentPrice}) => {
+//   comparePrice(currentPrice)
+// })
+.catch( (err) => {
+  console.log("error", err );
+});
